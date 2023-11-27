773705838b34991cba0cb4653374b50245d2104c
Abhishek kumar <abhishekhzb8@gmail.com>
2023-11-27 17:13:23 +0530

    Update CheckOddEven.java

--- CheckOddEven.java
+++ CheckOddEven.java
@@ -3,23 +3,15 @@ public class Demo {
         public static void main(String[] args) {
               
          Scanner scanner = new Scanner(System.in);
-        
          System.out.println("Input your number:");
          int a = scanner.nextInt();
         
          if(a %2==0) {
-               
                 System.out.println("Given number " +a +" is an even number");
-               
          } else {
-               
                 System.out.println("Given number " +a +" is an odd number");
-               
          }
-        
          scanner.close();
-        
-       
+                
        }
-
 }
