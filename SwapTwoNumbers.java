752c300373c5e71e382150db528d6795a10c3211
Abhishek kumar <abhishekhzb8@gmail.com>
2023-11-28 15:15:41 +0530

    Create SwapTwoNumbers.java

---
+++ SwapTwoNumbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SwapTwoNumbers {
+    public static void main(String[] args) {
+    
+    int a = 1;
+    int b = 2;
+    
+    int temp = b;
+    b = a;
+    a = temp;
+    
+    System.out.println("a : "+a);
+    System.out.println("b : "+b);
+ 
+  }
+}
