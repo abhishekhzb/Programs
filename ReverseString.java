1a94703f4f2f9956e647fa97d74b15dda5edeea0
Abhishek kumar <abhishekhzb8@gmail.com>
2023-11-30 17:15:12 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,1 +1,19 @@
+import java.util.*;
 
+public class ReverseString {
+    public static void main(String[] args) {
+    
+    String str = "Hello";
+    char[] ch = str.toCharArray();
+    String reverseStr = "";
+    
+    int size = str.length();
+    
+    for(int i = size-1; i>=0 ;i--){
+      reverseStr = reverseStr+ch[i];
+    }
+    
+    System.out.println(reverseStr);
+ 
+  }
+}
