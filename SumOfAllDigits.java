5209069ceb4f347f1a4b90aa60fc5dfbf68ead91
Abhishek kumar <abhishekhzb8@gmail.com>
2023-11-30 11:27:29 +0530

    Create SumOfAllDigits.java

---
+++ SumOfAllDigits.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class SumOfAllDigits {
+    public static void main(String[] args) {
+    
+    int number = 12345;
+    int sum = 0;
+    
+    while(number!=0){
+      sum = sum + number%10;
+      number = number/10;
+    }
+    
+    System.out.println("Sum is : "+sum);
+ 
+  }
+}
