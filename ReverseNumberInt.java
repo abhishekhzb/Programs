f52cb04fa9083771f6f92cd4240b5eae98d7eed0
Abhishek kumar <abhishekhzb8@gmail.com>
2023-12-02 21:35:57 +0530

    Create ReverseNumberInt.java

---
+++ ReverseNumberInt.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class ReverseNumberInt {
+    public static void main(String[] args) {
+     
+     int number = 12345;
+     int reverse = 0;
+     
+     while(number!=0){
+       int reminder = number %10;
+       reverse = reverse*10 + reminder;
+       number = number / 10;
+     }
+     
+     System.out.println(""+reverse);
+  }
+}
